d76db823d1ff66143c0ce9d7252f231d1f927f30
ray-odero93 <raymond.odero@student.moringaschool.com>
2021-06-13 17:02:21 +0300

    it will add scripts for the  customer testimonials

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('document').ready(function () {
+    $("#subscribe").click(function (event) {
+        event.preventDefault();
+        var subscribedemail = $("#emailaddress").val();
+        alert(subscribedemail + " has been successfully subscribed. Thank you for joining our mailing list. ");
+        $("#emailaddress").val(""); //clear input field after successfully subscribing.
+
+    });
+
+});
